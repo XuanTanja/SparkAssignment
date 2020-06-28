93533a8ccab3f36777b099f621593b6b9500c98a
Tanja <itisfun4fever@gmx.de>
2020-06-28 14:34:28 +0200

    Initial commit

---
+++ src/main/scala/de/hpi/spark_assignment/SimpleSpark.scala
@@ -0,0 +1,24 @@
+package de.hpi.spark_assignment
+
+import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
+
+//SINGLETON
+object SimpleSpark extends App {
+
+  override def main(args: Array[String]): Unit = {
+
+    // Turn off logging
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    println("-------------------------------------- Init ---------------------------------------------");
+  }
+}
---
+++ src/main/scala/de/hpi/spark_assignment/Sindy.scala
@@ -0,0 +1,11 @@
+package de.hpi.spark_assignment
+
+import org.apache.spark.sql.SparkSession
+
+object Sindy {
+
+  def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+
+    // TODO
+  }
+}
