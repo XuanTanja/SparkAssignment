cd040fb0f31bfea60a07ececc2989271ace8cd70
Tanja <anon@ynum.com>
2020-07-04 19:00:28 +0200

    Commented the Steps made in Sindy

--- src/main/scala/de/hpi/spark_assignment/Sindy.scala
+++ src/main/scala/de/hpi/spark_assignment/Sindy.scala
@@ -9,8 +9,6 @@ object Sindy {
     import spark.implicits._ //This is defined inside of the sql context class (therefore it needs to be imported after initializing spark session)
 
     //General idea: make use of data frames to modify and group cells, then convert to array/strings
-
-
     val datasets = inputs.map(filename => { //Importing data from csv's
       spark.read
         .option("inferSchema", "true")
@@ -21,11 +19,11 @@ object Sindy {
 
     val columns = datasets.flatMap(data => data.columns.map(data.select(_))) //get columns from dataset
 
+
+    // Step 1 : Input Tuples to Cells
     val cells = columns.map(col => col //entry of column is mapped to column name
      .map(row => (row.get(0).toString, row.schema.fieldNames.head))) //iterate through columns, and get each row of the column (row.get(0)) with the schema header names
     //this is a data set because of .map -> example: [_1: string, _2: string]
-
-
 
     val groupedCellsTuples = cells.reduce((cell1,cell2) => cell1.union(cell2)) //joining 2 cells at a time and calling reduce to combine cells with the lambda expression (function)
 
@@ -33,32 +31,31 @@ object Sindy {
     val groupedCellsDataFrame = groupedCellsTuples.toDF("value", "singletonType") //creating dataframe with header
 
 
-    //https://stackoverflow.com/questions/37737843/aggregating-multiple-columns-with-custom-function-in-spark
-    //https://bzhangusc.wordpress.com/2015/03/29/the-column-class/
-    //We get columns "value" with groupedCellsDataFrame("value")
+    // Step 2 : Cells to Aggregation
+    //We get columns "value" with groupedCellsDataFrame("value") and collect_set is used to collect values, See: https://stackoverflow.com/questions/37737843/aggregating-multiple-columns-with-custom-function-in-spark and https://bzhangusc.wordpress.com/2015/03/29/the-column-class/
     val groupedValueSingletonType = groupedCellsDataFrame //aggregated by values column  and list the singleton types without "singletonType" duplicates
       .groupBy(groupedCellsDataFrame("value")).agg(collect_set(groupedCellsDataFrame("singletonType")).as("aggregatedAttributeSet"))
-      //collect_set is used to collect values
     //Example: [25508.61,WrappedArray(L_EXTENDEDPRICE, O_TOTALPRICE)]
 
-    //Value is removed (value is just useful for grouping of singletonType names)
+
+    // Step 3 : Aggregation to Global Partitioning
+    // Done by Spark
+
+
+    // Step 4 : Partitioning to Attribute Sets
     val attributeSets = groupedValueSingletonType
-      .select("aggregatedAttributeSet")
+      .select("aggregatedAttributeSet")  //Value is not needed for further steps because just used as grouping key for the singletonTypes
       .distinct() //We remove duplicates because we are interested in inclusion dependencies (being sufficiently represented by one list)
       .as[Seq[String]] //We use a list (Seq is a list in scala) of all the strings
     //Example: List(O_ORDERKEY, P_PARTKEY, O_CUSTKEY, C_CUSTKEY)
 
 
-
-    //https://stackoverflow.com/questions/44436856/explode-array-data-into-rows-in-spark
-    //Explode is used to distribute data into rows
-    //Creates a new row for each element in the given array or map column
-    //explode creates a row with each combination of the list elements as keys
+    // Step 4 : Attribute Sets to Inclusion Lists
+    //Explode creates a row with each combination of the list elements as keys, see https://stackoverflow.com/questions/44436856/explode-array-data-into-rows-in-spark
     val combinationList = attributeSets
       .select(explode(attributeSets("aggregatedAttributeSet")).as("key"), col("aggregatedAttributeSet"))
       .as[(String, Seq[String])]
-    /*
-    //Example:
+    /* Example:
     (L_SUPPKEY,List(L_SUPPKEY, S_SUPPKEY, P_PARTKEY, O_CUSTKEY, C_CUSTKEY))
     (S_SUPPKEY,List(L_SUPPKEY, S_SUPPKEY, P_PARTKEY, O_CUSTKEY, C_CUSTKEY))
     (P_PARTKEY,List(L_SUPPKEY, S_SUPPKEY, P_PARTKEY, O_CUSTKEY, C_CUSTKEY))
@@ -66,38 +63,38 @@ object Sindy {
     (C_CUSTKEY,List(L_SUPPKEY, S_SUPPKEY, P_PARTKEY, O_CUSTKEY, C_CUSTKEY))
      */
 
-    //with map we return a collection of elements after applying lambda function
-    //https://stackoverflow.com/questions/12864505/how-can-i-idiomatically-remove-a-single-element-from-a-list-in-scala-and-close
-    //filtering out first element in array
+    //filtering out first element in array, See: https://stackoverflow.com/questions/12864505/how-can-i-idiomatically-remove-a-single-element-from-a-list-in-scala-and-close
     val inclusionList = combinationList
-      .map(row => (row._1, row._2.toList.filter(_ != row._1))) //row._1 is gettinng the first row (in python it would be row[0])
+      .map(row => (row._1, row._2.toList.filter(_ != row._1))) //row._1 is getting the first row (in python it would be row[0]), with map we return a collection of elements after applying lambda function
       .toDF("firstAttribute", "inclusionArray")
     //Example: [L_SUPPKEY,WrappedArray(S_SUPPKEY, P_PARTKEY, O_CUSTKEY, C_CUSTKEY)]
 
 
-    //We group all key values together
+    // Step 5 : Inclusion Lists to Partition
+    // Done by Spark
+
+
+    //Step 6 : Partition to Aggregate
     val groupedValueInclusionList = inclusionList
-      .groupBy(inclusionList("firstAttribute")).agg(collect_set(inclusionList("inclusionArray")).as("aggregatedAttributeSet"))
+      .groupBy(inclusionList("firstAttribute")).agg(collect_set(inclusionList("inclusionArray")).as("aggregatedAttributeSet"))  //We group all key values together and aggregate again on "firstAttribute"
       .as[(String,Seq[Seq[String]])]
     //Example: (L_SUPPKEY,List(List(O_ORDERKEY, S_SUPPKEY, P_PARTKEY, O_CUSTKEY, C_CUSTKEY), List(P_PARTKEY, S_SUPPKEY, C_CUSTKEY, L_PARTKEY), List(C_NATIONKEY, S_NATIONKEY, N_NATIONKEY, P_PARTKEY, S_SUPPKEY, P_SIZE, C_CUSTKEY, L_PARTKEY), List(S_NATIONKEY, N_REGIONKEY, P_PARTKEY, P_SIZE, R_REGIONKEY, L_PARTKEY, C_NATIONKEY, O_ORDERKEY, N_NATIONKEY, S_SUPPKEY, O_CUSTKEY, C_CUSTKEY, L_LINENUMBER), List(S_SUPPKEY, P_PARTKEY, P_SIZE, C_CUSTKEY, L_PARTKEY), List(S_SUPPKEY, P_PARTKEY, C_CUSTKEY), List(S_SUPPKEY, P_PARTKEY, P_SIZE, O_CUSTKEY, C_CUSTKEY, L_PARTKEY), List(C_NATIONKEY, S_NATIONKEY, N_NATIONKEY, S_SUPPKEY, P_PARTKEY, P_SIZE, C_CUSTKEY, L_PARTKEY), List(P_PARTKEY, S_SUPPKEY, O_CUSTKEY, C_CUSTKEY, L_PARTKEY), List(O_ORDERKEY, S_SUPPKEY, P_PARTKEY, P_SIZE, O_CUSTKEY, C_CUSTKEY, L_PARTKEY), List(O_ORDERKEY, P_PARTKEY, S_SUPPKEY, C_CUSTKEY, L_PARTKEY), List(C_NATIONKEY, S_NATIONKEY, N_REGIONKEY, O_ORDERKEY, N_NATIONKEY, S_SUPPKEY, P_PARTKEY, P_SIZE, R_REGIONKEY, C_CUSTKEY, L_LINENUMBER, L_PARTKEY), List(S_SUPPKEY, P_PARTKEY, O_CUSTKEY, C_CUSTKEY), List(O_ORDERKEY, S_SUPPKEY, P_PARTKEY, C_CUSTKEY, L_PARTKEY), List(O_ORDERKEY, S_SUPPKEY, P_PARTKEY, C_CUSTKEY), List(P_PARTKEY, S_SUPPKEY, O_CUSTKEY, C_CUSTKEY), List(S_SUPPKEY, P_PARTKEY, O_CUSTKEY, C_CUSTKEY, L_PARTKEY), List(C_NATIONKEY, S_NATIONKEY, N_NATIONKEY, S_SUPPKEY, P_PARTKEY, P_SIZE, O_CUSTKEY, C_CUSTKEY, L_PARTKEY), List(O_ORDERKEY, S_SUPPKEY, P_PARTKEY, P_SIZE, C_CUSTKEY, L_PARTKEY), List(O_ORDERKEY, P_PARTKEY, S_SUPPKEY, O_CUSTKEY, C_CUSTKEY, L_PARTKEY), List(C_NATIONKEY, S_NATIONKEY, O_ORDERKEY, N_NATIONKEY, S_SUPPKEY, P_PARTKEY, P_SIZE, O_CUSTKEY, C_CUSTKEY, L_LINENUMBER, L_PARTKEY), List(S_SUPPKEY, P_PARTKEY, C_CUSTKEY, L_PARTKEY), List(C_NATIONKEY, S_NATIONKEY, N_NATIONKEY, P_PARTKEY, S_SUPPKEY, P_SIZE, O_CUSTKEY, C_CUSTKEY, L_PARTKEY), List(O_ORDERKEY, P_PARTKEY, S_SUPPKEY, O_CUSTKEY, C_CUSTKEY), List(O_ORDERKEY, S_SUPPKEY, P_PARTKEY, O_CUSTKEY, C_CUSTKEY, L_PARTKEY), List(C_NATIONKEY, S_NATIONKEY, O_ORDERKEY, N_NATIONKEY, S_SUPPKEY, P_PARTKEY, P_SIZE, C_CUSTKEY, L_LINENUMBER, L_PARTKEY), List(P_PARTKEY, S_SUPPKEY, C_CUSTKEY)))
 
-    //Find intersection af all key values of aggregated inclusion list
+    //Find intersection af all key values of the aggregated inclusion list
     //Reduce just applies a function to a collection (row._2 in this case)
-    val partition = groupedValueInclusionList
+    val aggregate = groupedValueInclusionList
       .map(row => (row._1,row._2.reduce(_.intersect(_)))) //row._1 is getting the first row (in python it would be row[0])
       .filter(row=> row._2.nonEmpty) //remove empty lists
       .toDF("firstAttribute", "partitionArray")
 
-    val sortedPartition = partition
+    val sortedAggregation = aggregate
       .sort("firstAttribute") //sort alphabetically
       .as[(String,Seq[String])]
 
 
-    sortedPartition
+    // Step 7 : Aggregate to INDs
+    sortedAggregation
       .collect()
       .foreach(row => println(row._1 + " < " + row._2.reduce(_ + ", " + _)) )
-
-
-
   }
 }
