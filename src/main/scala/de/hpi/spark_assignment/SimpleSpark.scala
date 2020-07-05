a8ee35be9524a29fd49f54b1b5d547096d54c70e
samikreal <samikre@yahoo.com>
2020-07-05 14:37:05 +0200

    Added default parameters

--- src/main/scala/de/hpi/spark_assignment/SimpleSpark.scala
+++ src/main/scala/de/hpi/spark_assignment/SimpleSpark.scala
@@ -13,52 +13,66 @@ object SimpleSpark extends App {
 
     // java-jar SimpleSpark.jar --path data --cores 4
 
+    //TODO: hacer con length == 0 que use standard values de 32 cores
 
     val usage = "Usage: --path [folderDataPath] --cores [numCores]"
-    if (args.length == 0 || args.length <4 || args.length > 4) {
-      println(usage)
-    }
-    else if (args.length == 4){
-      if (args(0).equals("--path") && args(2).equals("--cores")){
-        //Run program
-        try {
-          var path = args(1)
-          var numCores = args(3).toInt
-          // Turn off logging in console
-          Logger.getLogger("org").setLevel(Level.OFF)
-          Logger.getLogger("akka").setLevel(Level.OFF)
 
-          println("------------------------------------ Initialize -------------------------------------------");
+    if (args.length == 0 ||args.length == 4){
+      //Default values if args.length == 0
+      var path = "/TPCH" //Default folder path of csv files
+      var numCores = 4
 
-          // Create a SparkSession to work with Spark (This is where tasks are distributed to Executors or workers)
-          val sparkBuilder = SparkSession
-            .builder()
-            .appName("SparkAssignment")
-            .master("local[" + numCores + "]") // local, with numCores worker cores
-          val spark = sparkBuilder.getOrCreate()
+      if (args.length > 0){
+        if (args(0).equals("--path") && args(2).equals("--cores")){
+          //Run program
+          try {
+            path = args(1)
+            numCores = args(3).toInt
+          }catch {
+            case error: NumberFormatException => {
+              println("--cores argument is not an integer")
+              return 0
+            }
 
-          // Set the default number of shuffle partitions (the default is 200)
-          //Shuffle partitions are the number of partitions that each data frame is divided and handled by an available core
-          spark.conf.set("spark.sql.shuffle.partitions", "8") //Does this have to do with num of cores??
-
-          //Get all csv files from folder
-          val dataArrayPaths: Array[File] = (new File(path + "/"))
-            .listFiles
-            .filter(_.toString.endsWith(".csv"))
-
-          val listStringPaths = dataArrayPaths //convert them to a list
-            .map(_.toString).toList
-
-          //Call Sindy to work on csv's, passing in spark session
-          Sindy.discoverINDs(listStringPaths, spark)
-
-        }catch {
-          case error: NumberFormatException => println("--cores argument is not an integer")
+          }
+        }
+        else {
+          print(usage)
+          return 0
         }
       }
-      else {
-        print(usage)
-      }
+
+      Logger.getLogger("org").setLevel(Level.OFF)
+      Logger.getLogger("akka").setLevel(Level.OFF)
+
+      println("------------------------------------ Initialize -------------------------------------------");
+
+      // Create a SparkSession to work with Spark (This is where tasks are distributed to Executors or workers)
+      val sparkBuilder = SparkSession
+        .builder()
+        .appName("SparkAssignment")
+        .master("local[" + numCores + "]") // local, with numCores worker cores
+      val spark = sparkBuilder.getOrCreate()
+
+      // Set the default number of shuffle partitions (the default is 200)
+      //Shuffle partitions are the number of partitions that each data frame is divided and handled by an available core
+      spark.conf.set("spark.sql.shuffle.partitions", "8") //Does this have to do with num of cores??
+
+      //Get all csv files from folder
+      val dataArrayPaths: Array[File] = (new File(path + "/"))
+        .listFiles
+        .filter(_.toString.endsWith(".csv"))
+
+      val listStringPaths = dataArrayPaths //convert them to a list
+        .map(_.toString).toList
+
+      //Call Sindy to work on csv's, passing in spark session
+      Sindy.discoverINDs(listStringPaths, spark)
+
     }
+    else{
+      println(usage)
+    }
+
   }
 }
